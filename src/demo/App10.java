0ff9905bae19343997ae42e97165ee58584ac69a
ziyinuo <1187900768@qq.com>
2017-03-29 23:28:34 +0800

    add equals rewrite

---
+++ src/demo/App10.java
@@ -0,0 +1,12 @@
+package demo;
+
+public class App10 {
+
+    public static void main(String[] args) {
+        Demo a = new Demo();
+        Demo b = new Demo();
+        System.out.println(a.equals(b));
+
+    }
+
+}
